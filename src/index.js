adbc9f2707d1c17b22fd41e7ed338bdb73ab9885
CharlotteMoureau <Charlotte.Moureau96@gmail.com>
2021-03-17 13:19:27 +0100

    js display

--- src/index.js
+++ src/index.js
@@ -1,44 +1,94 @@
+// buttons that the user can click
 const paperButton = document.getElementById('paper')
 const scissorsButton = document.getElementById('scissors')
 const rockButton = document.getElementById('rock')
 
-let userAnswer = ''
+let userAnswer = ""
 
-const computerAnswers = [1, 2, 3]
+// array with the choices of the cpu
+const computerAnswers = ["paper", "scissors", "rock"]
 
+// board game
+const gameBoard = document.getElementById('game')
+const vsBoard = document.getElementById('vs')
+
+// icons of the user displayed after he has clicked
+const userPaper = document.getElementById('user-paper')
+const userScissors = document.getElementById('user-scissors')
+const userRock = document.getElementById('user-rock')
+
+// score board
 const score = document.getElementById('score')
 let scoreCount = 0
 
+// timeout to launch the cpu choice
+let timeout
 
-function getUserAnswer() {
+
+// gets the choice of the user and displays them in the vs board then displays the cpu's
+function userSelected() {
     paperButton.addEventListener('click', function () {
-        userAnswer = 1
-    });
-
+        gameBoard.style.display = 'none'
+        vs.style.display = 'grid'
+        userPaper.style.display = 'block'
+        timeout = setTimeout(getComputerChoice, 1000)
+    })
     scissorsButton.addEventListener('click', function () {
-        userAnswer = 2
-    });
-
+        gameBoard.style.display = 'none'
+        vs.style.display = 'grid'
+        userScissors.style.display = 'block'
+        timeout = setTimeout(getComputerChoice, 1000)
+    })
     rockButton.addEventListener('click', function () {
-        userAnswer = 3
-    });
+        gameBoard.style.display = 'none'
+        vs.style.display = 'grid'
+        userRock.style.display = 'block'
+        timeout = setTimeout(getComputerChoice, 1000)
+    })
 }
 
-document.querySelectorAll('.button').forEach(element => {
-    element.addEventListener('click', function () {
+userSelected()
 
-        getUserAnswer()
-        const computerChoice = computerAnswers[Math.floor(Math.random() * computerAnswers.length)]
+// icons of the cpu displayed after the user has clicked
+const hollowCpu = document.getElementById('hollow')
+const cpuPaper = document.getElementById('cpu-paper')
+const cpuScissors = document.getElementById('cpu-scissors')
+const cpuRock = document.getElementById('cpu-rock')
 
 
-        if (userAnswer == computerChoice) {
-            console.log('tie')
-        } else if (userAnswer == 1 && computerChoice == 2 || userAnswer == 2 && computerChoice == 3 || userAnswer == 3 && computerChoice == 1) {
-            scoreCount = scoreCount - 1
-            score.innerText = scoreCount
-        } else {
-            scoreCount = scoreCount + 1
-            score.innerText = scoreCount
-        }
-    })
-});
+// makes the choice of the cpu random and displays it in the vs board
+function getComputerChoice() {
+    const choiceOfComputer = computerAnswers[Math.floor(Math.random() * computerAnswers.length)]
+    hollowCpu.style.display = 'none'
+    if (choiceOfComputer == 'paper') {
+        cpuPaper.style.display = 'block'
+        cpuPaper.animate([
+            // keyframes
+            { transform: 'scale(0.0)' },
+            { transform: 'scale(1.0)' }
+        ], {
+            // timing options
+            duration: 800,
+        });
+    } else if (choiceOfComputer == 'scissors') {
+        cpuScissors.style.display = 'block'
+        cpuScissors.animate([
+            // keyframes
+            { transform: 'scale(0.0)' },
+            { transform: 'scale(1.0)' }
+        ], {
+            // timing options
+            duration: 800,
+        });
+    } else {
+        cpuRock.style.display = 'block'
+        cpuRock.animate([
+            // keyframes
+            { transform: 'scale(0.0)' },
+            { transform: 'scale(1.0)' }
+        ], {
+            // timing options
+            duration: 800,
+        });
+    }
+}
